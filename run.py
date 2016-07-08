10b222d0a75c2938f1a35a54b1da2d02b3728855
Vincent Yang <VincentYang2014@gmail.com>
2016-07-08 01:26:03 -0700

    updated run msg

--- run.py
+++ run.py
@@ -4,12 +4,17 @@ from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """ respond to incoming calls with a simple text message. """
 
+    #resp = twilio.twiml.Response()
+    #resp.message = ("Hello, Mobile Monkey")
+    #return str(resp)
+
     resp = twilio.twiml.Response()
-    resp.message = ("Hello, Mobile Monkey")
+    resp.say("Hello Monkey")
     return str(resp)
 
 
