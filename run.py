e4c7168f62d826448b1731a5e81bfe1e68d6debb
Vincent Yang <VincentYang2014@gmail.com>
2016-07-08 00:52:32 -0700

    test req

--- run.py
+++ run.py
@@ -14,4 +14,6 @@ def hello_monkey():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
