4bf275bdb29934c2b8d5c17c741fb3d4bb939359
Vincent Yang <VincentYang2014@gmail.com>
2016-07-08 18:55:53 -0700

    python import bash env

--- text.py
+++ text.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from twilio.rest import TwilioRestClient
+import os
 
-account_sid = TWILIO_ACCT
-auth_token = TWILIO_TOKEN
+account_sid = os.environ.get("TWILIO_ACCT")
+auth_token = os.environ.get("TWILIO_TOKEN")
 client = TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(to="+14082032094", from_="+16503977854", body="hello from Vincent's Twilio!")
