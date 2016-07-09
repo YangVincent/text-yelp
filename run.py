3766831cc021c117c1ad3e3d4fb807bed63e9c5a
Vincent Yang <VincentYang2014@gmail.com>
2016-07-09 09:25:55 -0700

    update unit test nonetype

--- run.py
+++ run.py
@@ -23,10 +23,10 @@ callers = {
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    #""" respond to incoming calls with a simple text message. 
-    #>>> hello_monkey() != None
-    #True
-    #"""
+    """ respond to incoming calls with a simple text message. 
+    >>> hello_monkey() is None
+    False
+    """
     #'<?xml version="1.0" encoding="UTF-8"?><Response><Sms>SMS Hello Monkey</Sms></Response>'
 
     #from_number = request.values.get('From', None)
