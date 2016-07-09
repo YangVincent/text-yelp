08f6cfff9656741f3e26738f28ad6bb7eb8c5027
Vincent Yang <VincentYang2014@gmail.com>
2016-07-08 19:03:18 -0700

    test print env

--- text.py
+++ text.py
@@ -4,7 +4,9 @@ import os
 
 account_sid = os.environ.get("TWILIO_ACCT")
 auth_token = os.environ.get("TWILIO_TOKEN")
-client = TwilioRestClient(account=account_sid, token=auth_token)
+print(account_sid)
+print(auth_token)
+client = TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(to="+14082032094", from_="+16503977854", body="hello from Vincent's Twilio!")
 
